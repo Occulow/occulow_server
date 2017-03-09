f4a18ab47bc822896b1e2dbaf5c43ee9467d1426
Jacob Brooks <jacob@chrisbrooks.org>
2017-03-09 14:50:09 -0500

    Empty view

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'api'
 urlpatterns = [
-    
+    url(r'^sensors', views.list_sensors, name="list_sensors")
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotAllowed
+from . import models
 
-# Create your views here.
+only_get_allowed = HttpResponseNotAllowed(['GET'])
+
+# Sensor methods
+
+def list_sensors(request):
+    if request.method != 'GET':
+        return only_get_allowed
+
+    
