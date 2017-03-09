95cb131f28b8df1ba85e644c9b35d6edb3200a46
Jacob Brooks <jacob@chrisbrooks.org>
2017-03-09 14:35:00 -0500

    Dict methods for models

--- api/models.py
+++ api/models.py
@@ -4,6 +4,19 @@ class Sensor(models.Model):
     name = models.CharField(max_length=200)
     dev_eui = models.CharField(max_length=200)
 
+    def as_dict(self, with_relationships=False):
+        data = {
+            'type': 'sensor',
+            'name': self.name,
+            'dev_eui': self.dev_eui,
+        }
+
+        if with_relationships:
+            data['rooms'] = [r.as_dict() for r in self.roomsensor_set.all()]
+
+        return data
+
+
 class RoomSensor(models.Model):
     NEGATIVE = -1
     POSITIVE = 1
@@ -13,12 +26,46 @@ class RoomSensor(models.Model):
     room = models.ForeignKey("Room")
     polarity = models.IntegerField(choices=POLARITY_CHOICES, default=POSITIVE)
 
+    def as_dict(self):
+        data = {
+            'type': 'roomsensor',
+            'sensor': self.sensor.id,
+            'room': self.room.id,
+            'polarity': self.polarity
+        }
+
+        return data
+
 class Room(models.Model):
     name = models.CharField(max_length=200)
     sensors = models.ManyToManyField(Sensor, through=RoomSensor)
-    people = models.PositiveIntegerField()
+    count = models.PositiveIntegerField()
+
+    def as_dict(self, with_relationships=False):
+        data = {
+            'type': 'room',
+            'name': self.name,
+            'count': self.count
+        }
+
+        if with_relationships:
+            data['sensors'] = [s.as_dict() for s in self.sensors]
+
+        return data
 
 class Update(models.Model):
     value = models.IntegerField()
     time = models.DateTimeField(auto_now_add=True)
     sensor = models.ForeignKey(Sensor)
+
+    def as_dict(self, with_relationships=False):
+        data = {
+            'type': 'update',
+            'time': str(self.time),
+            'value': self.value,
+        }
+
+        if with_relationships:
+            data['sensor'] = self.sensor.as_dict()
+
+        return data
