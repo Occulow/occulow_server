0f99336d492d2613ebddb9c8e442ef011318f5e5
Jacob Brooks <jacob@chrisbrooks.org>
2017-03-09 13:30:22 -0500

    First pass at sensor models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Sensor(models.Model):
+    name = models.CharField(max_length=200)
+    dev_eui = models.CharField(max_length=200)
+
+class RoomSensor(models.Model):
+    NEGATIVE = -1
+    POSITIVE = 1
+    POLARITY_CHOICES = [(NEGATIVE,'Negative', (POSITIVE, 'Positive'))]
+
+    sensor = models.ForeignKey(Sensor)
+    room = models.ForeignKey(Sensor)
+    polarity = models.IntegerField(choices=POLARITY_CHOICES, default=RoomSensor.POSITIVE)
+
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    sensors = models.ManyToManyField(Sensor)
+
+class Update(models.Model):
+    value = models.IntegerField()
+    time = models.DateTimeField(auto_now_add=True)
+    sensor = models.ForeignKey(Sensor)
