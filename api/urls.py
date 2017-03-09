20552308b40ba2a2ed79def1404e3b24393ef57e
Jacob Brooks <jacob@chrisbrooks.org>
2017-03-09 16:00:48 -0500

    Method dispatcher for urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,20 @@
 from django.conf.urls import url
+from django.http import HttpResponseNotAllowed
 
 from . import views
 
+# Dispatches to view based on HTTP method
+def method_dispatch(**table):
+    def invalid_method(request, *args, **kwargs):
+        return HttpResponseNotAllowed(table.keys())
+
+    def d(request, *args, **kwargs):
+        handler = table.get(request.method, invalid_method)
+        return handler(request, *args, **kwargs)
+    return d
+
+
 app_name = 'api'
 urlpatterns = [
-    url(r'^sensors', views.list_sensors, name="list_sensors")
+    url(r'^sensors', method_dispatch(GET=views.list_sensors,POST=views.add_sensor), name="list_sensors"),
 ]
